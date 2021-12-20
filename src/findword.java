001012d0cc5caa659d7ac1fb4693f4c2c703c8a8
Silve45 <achijioke03@gmail.com>
2021-12-20 08:20:42 -0500

    adds the getAll method

--- src/findword.java
+++ src/findword.java
@@ -1,13 +1,19 @@
 //I pulled this from online and then I edited it
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class findword {
     public static void main(String args[]) throws FileNotFoundException {
         //Reading the word to be found from the user
+
+
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Enter the word to be found");
         String word = sc1.next();
+
+
+
         boolean flag = false;
         int count = 0;
         System.out.println("Contents of the line");
@@ -21,6 +27,10 @@ public class findword {
                 count = count+1;
             }
         }
+
+
+
+
         if(flag) {
             System.out.println("File contains the specified word");
             System.out.println("Number of occurrences is: "+count);
@@ -28,4 +38,14 @@ public class findword {
             System.out.println("File does not contain the specified word");
         }
     }
+
+    public void getAll (){
+        ArrayList<String> bacon = new ArrayList<String>();
+        for (int i = 0; i < bacon.size(); i++){
+            bacon.indexOf(i);
+            System.out.println(bacon.indexOf(i));
+        }
+    }
+
+
 }
